f136a9f3441ce2dc7ac23db71e1fd6ae01c5eb99
MONISHA A <136707691+Monishaappavu@users.noreply.github.com>
2023-10-13 11:23:26 +0530

    Add files via upload

---
+++ javascript-todo/javascript.js
@@ -0,0 +1,36 @@
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
+function inputLength(){
+	return input.value.length;
+} 
+function listLength(){
+	return item.length;}
+function createListElement() {
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	input.value = ""; //Reset text input field
+	function crossOut() {
+		li.classList.toggle("done");}
+	li.addEventListener("click",crossOut);
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+function deleteListItem(){
+		li.classList.add("delete")
+	}//END ADD CLASS DELETE
+}
+function addListAfterClick(){
+	if (inputLength() > 0) { //makes sure that an empty input field doesn't create a li
+		createListElement();
+	}}
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.which ===13) { //this now looks to see if you hit "enter"/"return"
+		//the 13 is the enter key's keycode, this could also be display by event.keyCode === 13
+		createListElement();
+	}}
+enterButton.addEventListener("click",addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
